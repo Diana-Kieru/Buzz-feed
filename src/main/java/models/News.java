69308f323bb2ee8c79ec31cd5921f86e61f11754
diana <dianakieru1@gmil.com>
2022-05-16 00:08:10 +0300

    added News content

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,43 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
+    private int id;
+    private String post;
+
+    public News(int id, String post) {
+        this.id = id;
+        this.post = post;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof News)) return false;
+        News that = (News) o;
+        return id == that.id &&
+                Objects.equals(post, that.post);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(post, id);
+    }
 }
+
