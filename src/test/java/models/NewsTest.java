93f0863e38ee2bf074ee6e8e4e72b295c36c9d35
diana <dianakieru1@gmil.com>
2022-05-19 12:01:35 +0300

    added changes to my DB

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,4 +1,25 @@
-package models;
-
-public class NewsTest {
-}
+//package models;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotEquals;
+//
+//public class NewsTest {
+//    @Test
+//    public void getPostReturnsCorrectPost() {
+//        News news = setupNews();
+//        assertEquals("BBI Demolition", news.getPost());
+//    }
+//
+//    @Test
+//    public void setPostSetsCorrectPost() throws Exception {
+//        News news =setupNews();
+//        news.setPost("Olympics");
+//        assertNotEquals("BBI Demolition",news.getPost());
+//    }
+//
+//    public News setupNews(){
+//        return new News("BBI Demolition");
+//    }
+//}
